d7f9f635f1fd58b3bc89674c00f09dc1c5dca69f
Danilo Herdy Oliveira <daniloho89@gmail.com>
2020-02-25 17:13:03 -0300

    Android 1 -  Aula 02
    Inclusão de ListView

--- Agenda/app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -2,15 +2,32 @@ package br.com.alura.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Danilo", Toast.LENGTH_LONG).show();
+        //Aula 02
+        //setContentView = Buscar a View da Activity
+        //R. = Resources
+        setContentView(R.layout.activity_main);
+        List<String> nomesAluno = new ArrayList<>(Arrays.asList("Danilo","Herdy","Oliveira","Ryuu"));
+        //Aula 02
+        // findViewById = Busca a View(View neste caso é o objetc (controle de tela))
+        ListView listaAlunos = findViewById(R.id.activity_main_lista_alunos);
+        //Aula 02
+        //android.R = Resources do pacote Android
+        //setAdapter = setando o Adapter necessario para o uso dinâmico
+        listaAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,nomesAluno));
+
     }
 }

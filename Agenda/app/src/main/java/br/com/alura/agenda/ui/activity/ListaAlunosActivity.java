8756532960f87f1ea54a7dc69dc8a488418128db
Danilo Herdy Oliveira <daniloho89@gmail.com>
2020-03-22 15:18:44 -0300

    Fundamentos pt3 - Aula 01
    Inserindo um Adapter Personalizado para a View

--- Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -3,10 +3,13 @@ package br.com.alura.agenda.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -15,11 +18,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.alura.agenda.R;
 import br.com.alura.agenda.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
+import br.com.alura.agenda.ui.adapter.ListaAlunosAdapter;
 
 import static br.com.alura.agenda.ui.activity.ConstantesActivies.CHAVE_ALUNO;
 
@@ -27,7 +32,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     public static final String TITULO_APPBAR = "Lista de Alunos";
     private final AlunoDAO dao = new AlunoDAO();
-    private ArrayAdapter<Aluno> adapter;
+    private ListaAlunosAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -116,7 +121,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
     }
 
     private void configuraAdapter(ListView listaAlunos) {
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        adapter = new ListaAlunosAdapter(this);
         listaAlunos.setAdapter(adapter);
     }
 }
